cdef3070d1833ddea98bce93be7d9f26650374ed
Katharina Muellek <katharina.muellek@hotmail.com>
2020-11-04 14:34:43 +0100

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Katharina Müllek on 04.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
